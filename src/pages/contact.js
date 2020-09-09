1f29f722916590d885d675ca95ddf49e493ff5ce
Liam Richards <liaminmanila@gmail.com>
2020-09-09 20:10:20 +0800

    fixed buttons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import { Typography } from "@material-ui/core"
+
+// var date1 = Date("04/06/2019")
+// var date2 = Date("09/09/2020")
+// var difference_In_Time = date2.getTime() - date1.getTime()
+// var difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)
 
 export default function Contact() {
   return (
     <div style={{ color: "teal" }}>
-      <Link to="/">Home</Link>
-      <Header headerText="The Message"></Header>
-      <p>You're looking cute ;)</p>
+      <Link to="/">Back to Donations</Link>
+      <Header headerText="Cause ahduntgeddit...how did a bombshell like you wind up liking me?"></Header>
+      {/* <p>How long we've been together: {Difference_In_Days} days</p> */}
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,53 +11,108 @@ import {
 } from "@material-ui/core"
 import { Link } from "gatsby"
 import { render } from "react-dom"
+import * as firebase from "firebase"
 
-var donations = 784
+var config = {
+  apiKey: "AIzaSyD2sXlNameacge0NKM9qcOZsMGIVDuai7A",
+  authDomain: "elf-withdrawals.firebaseapp.com",
+  databaseURL: "https://elf-withdrawals.firebaseio.com",
+  projectId: "elf-withdrawals",
+  storageBucket: "elf-withdrawals.appspot.com",
+  messagingSenderId: "151253140254",
+  appId: "1:151253140254:web:e02428ede5302701397f0f",
+  measurementId: "G-0YDKKJDQF4",
+}
+firebase.initializeApp(config)
+
+const db = firebase.firestore()
+const docRef = db.collection("data").doc("hugs")
+
+var donations = 0
+
+// setting the donations
+db.collection("data")
+  .doc("hugs")
+  .onSnapshot(function (doc) {
+    if (doc && doc.exists) {
+      // window.location.reload(false)
+      console.log("Current data: ", doc.data())
+      const myData = doc.data()
+      donations = myData.hugs
+    }
+  })
 
 function addOneHug(donations) {
   donations = donations + 1
-  // window.location.reload(false)
-  // print(donations)
-  // render()
+  console.log("Donations ", donations)
+  // Saving to database
+  db.collection("data").doc("hugs").set({
+    hugs: donations,
+  })
 }
 function addTenHugs(donations) {
   donations = donations + 10
-  // window.location.reload(false)
+  console.log("Donations ", donations)
+  // Saving to database
+  db.collection("data").doc("hugs").set({
+    hugs: donations,
+  })
 }
 function addHundredHugs(donations) {
   donations = donations + 100
-  // window.location.reload(false)
+  console.log("Donations ", donations)
+  // Saving to database
+  db.collection("data").doc("hugs").set({
+    hugs: donations,
+  })
+}
+
+function refreshPage() {
+  window.location.reload(false)
+}
+
+function getRealtimeUpdates() {
+  docRef.onSnapshot(function (doc) {
+    if (doc && doc.exists) {
+      // window.location.reload(false)
+      console.log("Current data: ", doc.data())
+      const myData = doc.data()
+      donations = myData.hugs
+    }
+  })
 }
 
 export default function Home() {
   return (
     // const hugs = {0}
     <div style={{ color: `purple` }}>
-      <Link to="/contact/">Click for a secret message</Link>
-
+      <Link to="/contact/">Are you a kilogram of steel or feathers?</Link>
       <Grid
         container
-        spacing={0}
+        spacing={1}
         direction="column"
         alignItems="center"
         justify="center"
         style={{ minHeight: "10vh" }}
       >
         <h1>The Elf Withdrawal Relief Fund</h1>
-        <p>A noble effort for Pandoalas to help out Koandas in need</p>
+        <p>A noble effort for Pandoalas to help out Koandas in need.</p>
         <Grid item xs={12}>
           <img src={pandaGIF} alt="Panda hug" />
         </Grid>
         <Grid item xs={12}>
-          <Typography variant="h4">Total Donations: {donations}</Typography>
+          <Typography id="totaldonations" variant="h4">
+            Total Donations: {donations}
+          </Typography>
         </Grid>
-      </Grid>
-      <Grid container alignItems="center" justify="center" spacing={0}>
         <Grid item xs={4}>
           <Button
             variant="contained"
             color="primary"
-            onClick={addOneHug(donations)}
+            onClick={() => {
+              addOneHug(donations)
+              getRealtimeUpdates()
+            }}
           >
             Donate 1 hug
           </Button>
@@ -66,7 +121,10 @@ export default function Home() {
           <Button
             variant="contained"
             color="primary"
-            onClick={addTenHugs(donations)}
+            onClick={() => {
+              addTenHugs(donations)
+              getRealtimeUpdates()
+            }}
           >
             Donate 10 hugs
           </Button>
@@ -75,9 +133,25 @@ export default function Home() {
           <Button
             variant="contained"
             color="primary"
-            onClick={addHundredHugs(donations)}
+            onClick={() => {
+              addHundredHugs(donations)
+              getRealtimeUpdates()
+              console.log("Local donations: ", donations)
+            }}
           >
             Donate 100 hugs
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            variant="contained"
+            color="green"
+            onClick={() => {
+              refreshPage()
+              // window.location.reload(true)
+            }}
+          >
+            Refresh
           </Button>
         </Grid>
       </Grid>
